06e0572edcccb933ec21e3dfdf4c6edd6f905629
hassan1324sa <139161457+hassan1324sa@users.noreply.github.com>
2024-01-04 11:14:00 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "list.h"
 #include <conio.h>
-#include<windows.h>
+#include <windows.h>
 #define BLK "\e[0;30m"
 #define RED "\e[0;31m"
 #define GRN "\e[0;32m"
@@ -15,12 +15,12 @@ void main()
     int choice;
     List l;
     createList(&l);
+     printf(""CYN);
     do
     {
         system("cls");
         MainDes();
-        // printf(BGRN);
-        printf("please enter choice \n");
+        printf("All Rights Reserved 2024 @Omar & @Hassan \n\n"RED"please enter choice \n");
         printf("\e[0;32m1-Insert Node  \n\e[0;36m2-Search Node \n\e[0;31m3-Delete Node\n\e[0;34m4-List size \n\e[0;32m5-Print List \n\e[0;31m6-Destroy list \n\e[0;36m7-Sort \n\e[0;32m8-Edit \n\e[0;36m9-Rank \n\e[0;31m10-average \n\e[0m11-EXIT \n\e"MAG"->");
         scanf("%d", &choice);
         fflush(stdin);
@@ -65,7 +65,7 @@ void main()
         default:
             if (choice == EXIT)
             {
-            int x= MessageBoxA( NULL, "This Project Made by Omar Adel & Hassan Mohamed\nHope it likes You,See You later <3", "Good bye!", MB_ICONASTERISK | MB_OK );
+            int x= MessageBoxA( NULL, "This Project is Made by Omar Adel & Hassan Mohamed\nHope it likes You,See You later <3", "All Rights Reserved \xA9 2024 @Omar & @Hassan", MB_ICONASTERISK | MB_OK );
             while (x==!IDOK);
             system("cls");
                 return;
